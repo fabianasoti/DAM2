79f655a0e0a1691453ba5e7a08c7ea6b05bc8481
fabianasoti <fsotillocuevas@gmail.com>
2026-09-22 18:15:57 +0200

    Create registro_notas.py

---
+++ 001-Acceso a datos/001-Manejo de ficheros/001-Clases asociadas a las operaciones de gestión de ficheros/gestion_archivos/ejercicio_notas/registro_notas.py
@@ -0,0 +1,32 @@
+import os
+ 
+NOMBRE_FICHERO_NOTAS = "notas.csv"
+CARPETA_PRACTICAS = "practicas"
+NOMBRE_FICHERO_ESTRUCTURA = "estructura_practicas.txt"
+ 
+def guardar_notas():
+    # tu codigo aqui
+    pass
+ 
+def leer_notas():
+    # tu codigo aqui
+    pass
+ 
+def crear_carpetas_de_prueba():
+    # tu codigo aqui
+    pass
+ 
+def dibujar_estructura_practicas(ruta):
+    # tu codigo aqui
+    pass
+ 
+def guardar_estructura_practicas(ruta, archivo_salida=NOMBRE_FICHERO_ESTRUCTURA):
+    # tu codigo aqui
+    pass
+ 
+def main():
+    # llama aqui a las funciones anteriores, en orden
+    pass
+ 
+if __name__ == "__main__":
+    main()
